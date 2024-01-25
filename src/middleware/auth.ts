f211b0e6a361f5ef709c77238b255d7053754453
Visions-Nicolas <nicolas@visionspol.eu>
2024-01-25 18:27:30 +0100

    feat: change auth middleware to allow participant to authenticate with api key

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import Participant from "../models/Participant/Participant.model";
 
-export const verifyParticipantJWT = (
+type DecodedServiceJWT = {
+  serviceKey: string;
+};
+export const verifyParticipantJWT = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -18,24 +22,71 @@ export const verifyParticipantJWT = (
       .json({ message: "Authorization header missing or invalid" });
   }
 
-  const token = authHeader.slice(7);
+  const authorization = req.header("authorization");
 
-  try {
-    const decodedToken = jwt.verify(
-      token,
-      process.env.JWT_SECRET_KEY
-    ) as JwtPayload;
+  let token;
 
-    req.decodedToken = decodedToken;
-    req.userParticipant = {
-      id: decodedToken.sub,
-    };
-    req.session.userParticipant = { id: decodedToken.sub };
+  token = authorization.split(" ")[1];
 
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token" });
+  const data = token.split(".");
+
+  if (data.length < 3) {
+    return res.status(401).json({
+      message: "'" + token + "' is not a valid authorization token",
+    });
   }
+
+  const buff = Buffer.from(data[1], "base64");
+  const authObject: DecodedServiceJWT = JSON.parse(buff.toString());
+
+  if (!authObject.serviceKey) {
+    token = authHeader.slice(7);
+    try {
+      const decodedToken = jwt.verify(
+        token,
+        process.env.JWT_SECRET_KEY
+      ) as JwtPayload;
+
+      req.decodedToken = decodedToken;
+      req.userParticipant = {
+        id: decodedToken.sub,
+      };
+      req.session.userParticipant = { id: decodedToken.sub };
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid or expired token" });
+    }
+  } else {
+    const { serviceKey } = authObject;
+
+    const participant = await Participant.findOne({
+      clientID: serviceKey,
+    }).lean();
+
+    if (!participant)
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized resource" });
+
+    try {
+      const decoded = jwt.verify(token, participant.clientSecret);
+      if (decoded) {
+        return next();
+      } else {
+        return res.status(401).json({
+          error: "token-decode-error",
+          message: "Unauthorized resource",
+        });
+      }
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ error: error, message: "Unauthorized resource" });
+    }
+  }
+
+  return res.status(200).json({ message: "ok" });
 };
 
 export const verifyUserJWT = (
