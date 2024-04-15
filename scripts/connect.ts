adadfc2a0727043e428a55274a91e764d18aaf96
Visions-Nicolas <nicolas@visionspol.eu>
2024-04-15 14:35:18 +0200

    feat : script update

--- scripts/connect.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connect = async () => {
-  if (!process.env.MONGO_URI) throw new Error("Missing MONGO_URI");
-  await mongoose.connect(process.env.MONGO_URI);
-};
