ddb8c84b8309c59bd6d8baf6e851164bd89ad5f4
FelixBole <54702474+FelixBole@users.noreply.github.com>
2024-07-01 17:11:45 +0200

    Revert "Unit test update"

--- src/tests/users.spec.ts
+++ src/tests/users.spec.ts
@@ -4,7 +4,6 @@ import { Application } from "express";
 import { startServer } from "../server";
 import { IncomingMessage, ServerResponse } from "http";
 import * as http from "http";
-import { testParticipant3, testUser2 } from "./fixtures/testAccount";
 
 let serverInstance: {
   app: Application;
@@ -15,18 +14,36 @@ let participantJWT: string;
 before(async () => {
   serverInstance = startServer(9092);
 
-  //create new participant
-  const participantData = testParticipant3;
   const response = await supertest(serverInstance.app)
     .post(`/v1/participants`)
-    .send(participantData);
-  //login the participant
+    .send({
+      legalName: "consumer",
+      identifier: "656dfb3e282d47cfa6b66b2a",
+      did: "https://api.test.com/v1/catalog/participants/656dfb3e282d47cfa6b66b2a",
+      selfDescriptionURL:
+        "https://api.test.com/v1/catalog/participants/656dfb3e282d47cfa6b66b2a",
+      email: "provider@email.com",
+      endpoints: {
+        dataExport: "https://test.consent/data/export",
+        dataImport: "https://test.consent/data/import",
+        consentImport: "https://test.consent/consent/import",
+        consentExport: "https://test.consent/consent/export",
+      },
+      clientID:
+        "EAUh5wKsNzV2jhpxXYys6S6GyqtmTL05SN4JKxy6txuwKWzX1EWCMAA6hljz9kLBxHz5zkEdwWFFDE35bbKX6bqLVv75o7mMMAnw",
+      clientSecret:
+        "5bBtpV9bH0CkTr92bLar4zrWS6tAXZ6dUNSCNyRK7QtzA9hvX8n8nGyQSXY6hyhiochOrHFAN4KHSY0RPYhxWvW2Ao5TDE70ZmzJ",
+    });
+
   const authResponse = await supertest(serverInstance.app)
     .post(`/v1/participants/login`)
     .send({
-      clientID: testParticipant3.clientID,
-      clientSecret: testParticipant3.clientSecret,
+      clientID:
+        "EAUh5wKsNzV2jhpxXYys6S6GyqtmTL05SN4JKxy6txuwKWzX1EWCMAA6hljz9kLBxHz5zkEdwWFFDE35bbKX6bqLVv75o7mMMAnw",
+      clientSecret:
+        "5bBtpV9bH0CkTr92bLar4zrWS6tAXZ6dUNSCNyRK7QtzA9hvX8n8nGyQSXY6hyhiochOrHFAN4KHSY0RPYhxWvW2Ao5TDE70ZmzJ",
     });
+
   participantJWT = `Bearer ${authResponse.body.jwt}`;
 });
 
@@ -39,10 +56,14 @@ describe("Users Routes Tests", () => {
 
   describe("signup", () => {
     it("should create a new user", async () => {
-      const userData = testUser2;
       const response = await supertest(serverInstance.app)
         .post(`/v1/users/signup`)
-        .send(userData);
+        .send({
+          firstName: "John",
+          lastName: "Doe",
+          email: "john@example.com",
+          password: "password",
+        });
       userId = response.body.user._id;
       expect(response.status).to.be.equal(200);
       expect(response.body).to.have.property("user");
@@ -54,15 +75,15 @@ describe("Users Routes Tests", () => {
     });
   });
 
-  describe("login user", () => {
+  describe("login", () => {
     it("should login an existing user", async () => {
       const response = await supertest(serverInstance.app)
         .post(`/v1/users/login`)
         .send({
-          email: testUser2.email,
-          password: testUser2.password,
-        })
-        .expect(200);
+          email: "john@example.com",
+          password: "password",
+        });
+      expect(response.status).to.be.equal(200);
       expect(response.body).to.have.property("user");
       expect(response.body.user).to.have.property("firstName", "John");
       expect(response.body.user).to.have.property("lastName", "Doe");
@@ -77,8 +98,8 @@ describe("Users Routes Tests", () => {
         .send({
           email: "john@example.com",
           password: "wrongpassword",
-        })
-        .expect(400);
+        });
+      expect(response.status).to.be.equal(400);
       expect(response.body).to.have.property("message", "invalid credentials");
     });
 
@@ -88,25 +109,28 @@ describe("Users Routes Tests", () => {
         .send({
           email: "nonexisting@example.com",
           password: "password",
-        })
-        .expect(404);
+        });
+      expect(response.status).to.be.equal(404);
       expect(response.body).to.have.property("message", "User not found");
     });
   });
 
   describe("getUserById", () => {
     it("should get user by ID", async () => {
-      const response = await supertest(serverInstance.app)
-        .get(`/v1/users/${userId}`)
-        .expect(200);
+      // Assuming there's a user with ID 'userId123' in the database
+      const response = await supertest(serverInstance.app).get(
+        `/v1/users/${userId}`
+      );
+      expect(response.status).to.be.equal(200);
       expect(response.body).to.not.be.empty;
       // Assert other properties if needed
     });
 
     it("should fail to get non-existing user", async () => {
-      const response = await supertest(serverInstance.app)
-        .get(`/v1/users/65eed1cd59f9242b784f2494`)
-        .expect(404);
+      const response = await supertest(serverInstance.app).get(
+        `/v1/users/65eed1cd59f9242b784f2494`
+      );
+      expect(response.status).to.be.equal(404);
       expect(response.body).to.have.property("message", "User not found");
     });
   });
@@ -119,64 +143,34 @@ describe("Users Routes Tests", () => {
         .send({
           email: "newuser@example.com",
           identifier: "newidentifier",
-        })
-        .expect(200);
-      expect(response.body).to.not.be.empty;
-    });
+        });
 
-    it("should fail to register existing user identifier", async () => {
-      const response = await supertest(serverInstance.app)
-        .post(`/v1/users/register`)
-        .set("Authorization", participantJWT)
-        .send({
-          email: "newuser@example.com",
-          identifier: "newidentifier",
-        })
-        .expect(409);
-      expect(response.body).to.have.property("error", "User already exists");
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.not.be.empty;
     });
 
     it("should fail to register user identifier with missing fields", async () => {
       const response = await supertest(serverInstance.app)
         .post(`/v1/users/register`)
         .set("Authorization", participantJWT)
-        .send({})
-        .expect(400);
+        .send({});
       expect(response).to.throws;
+      expect(response.status).to.be.equal(400);
       expect(response.body).to.have.property("errors");
     });
 
-    it("should fail to register user without email and identifier", async () => {
+    it("should fail to register existing user identifier", async () => {
+      // Assuming the identifier already exists in the database
       const response = await supertest(serverInstance.app)
         .post(`/v1/users/register`)
         .set("Authorization", participantJWT)
         .send({
-          users: [{}],
-        })
-        .expect(400);
-      expect(response.body).to.have.property("errors");
-      expect(response.body.errors[0]).to.deep.include({
-        field: "email",
-        message: "Email must exist if identifier does not",
-      });
-      expect(response.body.errors[1]).to.deep.include({
-        field: "identifier",
-        message: "identifier must exist if email does not",
-      });
+          email: "newuser@example.com",
+          identifier: "newidentifier",
+        });
+      expect(response.status).to.be.equal(409);
+      expect(response.body).to.have.property("error");
     });
-
-    // it("should fail to register user identifier with invalid email format", async () => {
-    //   const response = await supertest(serverInstance.app)
-    //     .post(`/v1/users/register`)
-    //     .set("Authorization", participantJWT)
-    //     .send({
-    //       email: "invalid-email",
-    //       identifier: "newidentifier",
-    //     })
-    //     .expect(400);
-    //   expect(response).to.throws;
-    //   expect(response.body).to.have.property("error", "Invalid email format");
-    // });
   });
 
   describe("registerUserIdentifiers", () => {
@@ -189,64 +183,36 @@ describe("Users Routes Tests", () => {
             { email: "user1@example.com", internalID: "id1" },
             { email: "user2@example.com", internalID: "id2" },
           ],
-        })
-        .expect(200);
+        });
+      expect(response.status).to.be.equal(200);
       expect(response.body).to.not.be.empty;
       // Assert other properties if needed
     });
 
+    it("should fail to register user identifiers with missing fields", async () => {
+      const response = await supertest(serverInstance.app)
+        .post(`/v1/users/registers`)
+        .set("Authorization", participantJWT)
+        .send({
+          users: [{ internalID: "id1" }, { email: "user2@example.com" }],
+        });
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.have.property("errors");
+    });
+
     it("should not register existing user identifiers", async () => {
+      // Assuming the identifiers already exist in the database
       const response = await supertest(serverInstance.app)
         .post(`/v1/users/registers`)
         .set("Authorization", participantJWT)
         .send({
           users: [
-            { email: "user1@example.com", internalID: "id1" },
-            { email: "user2@example.com", internalID: "id2" },
+            { email: "existinguser1@example.com", internalID: "existingid1" },
+            { email: "existinguser2@example.com", internalID: "existingid2" },
           ],
         });
-      expect(400);
-      //error message inapropriate
-    });
-
-    it("should fail to register user identifiers with missing fields", async () => {
-      const response = await supertest(serverInstance.app)
-        .post(`/v1/users/register`)
-        .set("Authorization", participantJWT)
-        .send({})
-        .expect(400);
-      expect(response).to.throws;
-      expect(response.body).to.have.property(
-        "error",
-        "Missing or invalid fields"
-      );
-      expect(response.body).to.have.property("errors").that.is.an("array");
-      expect(response.body.errors).to.deep.include({
-        field: "email",
-        message: "Email must exist if identifier does not",
-      });
-      expect(response.body.errors).to.deep.include({
-        field: "identifier",
-        message: "identifier must exist if email does not",
-      });
-    });
-    it("should fail to register user without email and identifier", async () => {
-      const response = await supertest(serverInstance.app)
-        .post(`/v1/users/registers`)
-        .set("Authorization", participantJWT)
-        .send({
-          users: [{}],
-        })
-        .expect(400);
-      expect(response.body).to.have.property("errors");
-      expect(response.body.errors[0]).to.deep.include({
-        field: "email",
-        message: "Email must exist if identifier does not",
-      });
-      expect(response.body.errors[1]).to.deep.include({
-        field: "identifier",
-        message: "identifier must exist if email does not",
-      });
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.not.be.empty;
     });
   });
 });
